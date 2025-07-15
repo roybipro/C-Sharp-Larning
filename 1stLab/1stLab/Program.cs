b89de973f522bb314ee285a6a3db7df57e896549
Bipro Roy <roybipro896@gmail.com>
2025-07-15 10:10:49 +0600

    a

---
+++ 1stLab/1stLab/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 17, b = 6;
+        int sum = a + b;
+        double divide = a / (b + 0.0); //typle casting (double)b
+        Console.WriteLine("The Sum of {0} and {1} is: {2}", a, b, sum); //{}callded place holder
+        Console.WriteLine($"The Sum of {a} and {b} is: {sum}");//string interpolatition
+        Console.WriteLine("divition of " + a + " and " + b + " is " + divide);
+
+
+        //5
+        int sum2 = a + b;
+        int multiply = a * b;
+        int subtract = a - b;
+        int divide2 = a / b;
+        Console.WriteLine(sum2);
+        Console.WriteLine(multiply);
+        Console.WriteLine(subtract);
+        Console.WriteLine(divide2);
+
+        //7
+        double width = 5.5;
+        double hight = 8.5;
+        double area = a * b;
+        double perameter = 2 * (width + hight);
+
+        Console.WriteLine("The area is: " + area);
+        Console.WriteLine("The Perameter: " + perameter);
+
+
+
+    }
+}
+
+
+
